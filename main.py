acd9acb9b1b6094910195d3259a7b67269700e88
4321louis <31239298+4321louis@users.noreply.github.com>
2021-01-01 01:21:08 +1100

    Sad circular import resolving
    Just get rid of utils at this point

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ from discord.ext import commands
 import asyncio
 
 
+def json_encoder(obj: Object):
+    if isinstance(obj,Guild) or isinstance(obj,VoiceChannel):
+        return obj.id
+    if isinstance(obj,InfinityVoice):
+        return obj.__dict__
+
+def json_decoder(str:str) -> dict:
+    loaded = json.loads(str)
+    final = []
+    for i in loaded:
+        final.append(InfinityVoice(bot.get_guild(i["guild"]),i["name_format"],i["user_limit"]))
+        for j in i["channels"]:
+            final[-1].active_channels.append(bot.get_channel(j))
+    return final
+
 #create bot instance
 bot = commands.Bot(command_prefix='?')
 
--- utils.py
+++ utils.py
@@ -1,23 +1,2 @@
-from discord import *
-from datetime import datetime,timedelta
-import json
-from InfinityVoice import InfinityVoice
-from main import bot
-
-def json_encoder(obj: Object):
-    if isinstance(obj,Guild) or isinstance(obj,VoiceChannel):
-        return obj.id
-    if isinstance(obj,InfinityVoice):
-        return obj.__dict__
-
-def json_decoder(str:str) -> dict:
-    loaded = json.loads(str)
-    final = []
-    for i in loaded:
-        final.append(InfinityVoice(bot.get_guild(i["guild"]),i["name_format"],i["user_limit"]))
-        for j in i["channels"]:
-            final[-1].active_channels.append(bot.get_channel(j))
-    return final
-
 def print_timed(s: str) -> None:
     print("[" + str(datetime.now()) + "]" + s)   
