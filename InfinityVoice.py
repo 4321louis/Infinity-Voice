d09b0613d9f12f28517c0721a95d20d9884129d1
4321louis <31239298+4321louis@users.noreply.github.com>
2021-01-01 20:29:41 +1100

    Simplifing update_channels() + ChannelOverride

--- InfinityVoice.py
+++ InfinityVoice.py
@@ -1,51 +1,44 @@
-from utils import print_timed,json_encoder
+from utils import print_timed,ChannelOverride
 from main import bot
 import json
 from typing import Dict
+from functools import reduce
 
 global infinityVoices
-infinityVoices = defaultdict()[int,InfinityVoice]
+#guild id to infity voice
+infinityVoices = Dict()[int,InfinityVoice]
 
 class InfinityVoice:    
     def __init__(self, guild: Guild, name_format: str, user_limit: int):
         self.guild = guild
         self.active_channels = []
-        #default channel override is stored in -1
-        self.overrides = {}
+        self.overrides = defaultdict()[int,ChannelOverride]
 
     #called to update the infinity voice 
     async def update_channels(self):
-        #check for one empty channel
-        found_empty = False
-        i = 0
+        #delete excess channels
+        found_empty= False
+        i=0
         while i < len(self.active_channels):
             if self.active_channels[i].members == []:
                 if found_empty == True:
                     await (self.active_channels.pop(i)).delete(reason = "Clearing Empty voice channels")
-                    i-=1
+                    continue
                 found_empty = True
             i+=1
-        
+        #create empty channel
         if found_empty==False:
-            if self.active_channels != []:
-                number = len(self.active_channels) + 1
-                self.active_channels.append(await self.guild.create_voice_channel(
-                    self.overrides.get(number).name_format.format(number),
-                    user_limit=self.overrides.get(number).user_limit,
-                    overwrites=self.overrides.get(number).overwrites,
-                    category=self.overrides.get(None).category,
-                    position=self.active_channels[-1].position))
-
-            else:
-                self.active_channels.append(await self.guild.create_voice_channel(self.name_format.format(len(self.active_channels)+1),user_limit=self.user_limit))
-        
+            number = len(self.active_channels)
+            self.active_channels.append(await self.guild.create_voice_channel(
+                self.overrides.get(number).name_format.format(number+1),
+                user_limit=self.overrides.get(number).user_limit,
+                overwrites=self.overrides.get(number).overwrites,
+                category=self.overrides.get(None).category,
+                position=self.overrides.get(None).position + len(active_channels) - 1))
         #rename channels
         for i in range(len(self.active_channels)):
-            if i in self.custom_names:
-                if self.active_channels[i].name != self.custom_names(i):
-                    await self.active_channels[i].edit(name =  self.custom_names(i))
-            elif self.active_channels[i].name != self.name_format.format(i+1):
-                await self.active_channels[i].edit(name =  self.name_format.format(i+1))
+            if self.overrides.get(i).name_format.format(i+1) != self.active_channels[i].name
+                    await self.active_channels[i].edit(name =  self.overrides.get(i).name_format.format(i+1))
 
 
     #updates the references for the channels in the infinity voice 'infinity_voice'
--- utils.py
+++ utils.py
@@ -1,2 +1,16 @@
 def print_timed(s: str) -> None:
     print("[" + str(datetime.now()) + "]" + s)   
+
+class ChannelOverride():
+    """
+    docstring
+    """
+    def __init__(self,name_format,limit,overwrites,position):
+        """
+        docstring
+        """
+        self.name_format = name_format
+        self.limit = limit
+        self.overwrites = overwrites
+        self.position = position
+
